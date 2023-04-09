d36d00dfbaf565e308b5247a7b8cee635eab3306
AlexWeapons <alexweapons_xd@hotmail.com>
2023-04-08 22:54:55 -0600

    d

--- autos-frontend/src/app/marcas/marca.service.ts
+++ autos-frontend/src/app/marcas/marca.service.ts
@@ -2,18 +2,17 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Marca } from './marca';
 import { Submarca } from '../submarcas/submarca';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MarcaService {
+  private urlEndpoint:string = 'http://localhost:8080/api/marcas';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getMarcas(): Observable<Marca[]> {
-    return of ([
-      {id: 1, nombre: 'Audi', submarcas: [new Submarca('R8'), new Submarca('A1'), new Submarca('A2')]},
-      {id: 2, nombre: 'BMW', submarcas: [new Submarca('Serie 1'), new Submarca('Serie 2'), new Submarca('Serie 3')]}
-    ]);
+    return this.http.get<Marca[]>(this.urlEndpoint)
   }
 }
