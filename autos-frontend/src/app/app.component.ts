93a7be448cc4d3dd83827d011122fb30e43e8d55
AlexWeapons <alexweapons_xd@hotmail.com>
2023-04-09 18:10:53 -0600

    UpdateHTML

--- autos-frontend/src/app/app.component.ts
+++ autos-frontend/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Bienvenido al mundo de los autos';
+  title = 'Cotizador de autos';
   autor = 'Alexis Armas';
 }
